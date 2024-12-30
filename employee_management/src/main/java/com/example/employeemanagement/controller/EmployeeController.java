84d8e121b4689e7da9b585ec377991f83e90a92d
Vasamsetti Prasanna kumar <137802131+Prasanna818504@users.noreply.github.com>
2024-12-30 11:05:21 +0530

    Add files via upload

---
+++ employee_management/src/main/java/com/example/employeemanagement/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package com.example.employeemanagement.controller;
+
+import com.example.employeemanagement.entity.Employee;
+
+import com.example.employeemanagement.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+@CrossOrigin(origins = "http://localhost:4200")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+    }
+}
